cde7d2004e2d691ca95c1cad81502848528d316c
MxCoffee <tcoffey114@gmail.com>
2016-09-24 15:24:02 -0400

    Created CardCountHand.cs

---
+++ CardCountHand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BlackJack
+{
+	class CardCountHand : Hand
+	{
+		public CardCountHand ()
+		{
+		}
+		abstract int EvaluateHand() //evaluates the hand, returns an integer corresponding to the rating of the hand.
+		{
+
+		}
+		abstract int CompareTo(Hand OtherHandObject) //compares two hands
+		{
+
+
+		}
+	}
+}
+
