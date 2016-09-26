d24d9888d862e656cf04e59416dc934b125295c6
MxCoffee <tcoffey114@gmail.com>
2016-09-26 02:03:17 -0400

    Get the symbol to get value and evaluate

--- BlackJackHand.cs
+++ BlackJackHand.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,63 +6,68 @@ namespace BlackJack
 {
 	class BlackJackHand : Hand
 	{
+		public BlackJackHand ()
+		{
+
+		}
 		
 		public override int EvaluateHand() //evaluates the hand, returns an integer corresponding to the rating of the hand.
 		{
 			int rating = 0;
 			foreach (Card x in hand) 
 			{
-				if(x.ToString().Contains("ACE"))
-				{
-					rating += 1;
-				}
-				if(x.ToString().Contains("TWO"))
+				
+				if(x.GetRank().GetSymbol() == "2")
 				{
 					rating += 2;
 				}
-				if(x.ToString().Contains("THREE"))
+				if(x.GetRank().GetSymbol() == "3")
 				{
 					rating += 3;
 				}
-				if(x.ToString().Contains("FOUR"))
+				if(x.GetRank().GetSymbol() == "4")
 				{
 					rating += 4;
 				}
-				if(x.ToString().Contains("FIVE"))
+				if(x.GetRank().GetSymbol() == "5")
 				{
 					rating += 5;
 				}
-				if(x.ToString().Contains("SIX"))
+				if(x.GetRank().GetSymbol() == "6")
 				{
 					rating += 6;
 				}
-				if(x.ToString().Contains("SEVEN"))
+				if(x.GetRank().GetSymbol() == "7")
 				{
 					rating += 7;
 				}
-				if(x.ToString().Contains("EIGHT"))
+				if(x.GetRank().GetSymbol() == "8")
 				{
 					rating += 8;
 				}
-				if(x.ToString().Contains("NINE"))
+				if(x.GetRank().GetSymbol() == "9")
 				{
 					rating += 9;
 				}
-				if(x.ToString().Contains("TEN"))
+				if(x.GetRank().GetSymbol() == "10")
 				{
 					rating += 10;
 				}
-				if(x.ToString().Contains("JACK"))
+				if(x.GetRank().GetSymbol() == "J")
 				{
 					rating += 10;
 				}
-				if(x.ToString().Contains("QUEEN"))
+				if(x.GetRank().GetSymbol() == "Q")
 				{
 					rating += 10;
 				}
-				if(x.ToString().Contains("KING"))
+				if(x.GetRank().GetSymbol() == "K")
 				{
 					rating += 10;
+				}
+				if(x.GetRank().GetSymbol() == "A")
+				{
+					rating += 11;
 				}
 			}	
 			return rating;
@@ -81,10 +86,7 @@ namespace BlackJack
 				return 0;
 			}
 		}
-		public BlackJackHand ()
-		{
-			
-		}
+
 
 	}
 }
